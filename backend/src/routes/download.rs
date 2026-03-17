5df93da3b0ffae35475848db101b19be8eef6b55
Taxrosdev <taxrosdev@mailbox.org>
2026-03-17 17:24:03 +1300

    feat: filenames on download

--- backend/src/routes/download.rs
+++ backend/src/routes/download.rs
@@ -1,10 +1,13 @@
 use crate::DATABASE;
 use crate::util::get_path;
 
-use axum::{extract::Path, http::StatusCode};
+use axum::{
+    extract::Path,
+    http::{HeaderMap, HeaderValue, StatusCode, header},
+};
 use tokio::fs;
 
-pub async fn download(Path(id): Path<String>) -> Result<Vec<u8>, StatusCode> {
+pub async fn download(Path(id): Path<String>) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
     let upload = DATABASE.get_upload(&id).map_err(|e| {
         eprintln!("{e}");
         StatusCode::INTERNAL_SERVER_ERROR
@@ -17,6 +20,13 @@ pub async fn download(Path(id): Path<String>) -> Result<Vec<u8>, StatusCode> {
         return Err(StatusCode::NOT_FOUND);
     }
 
+    // Set Content-Disposition header so filenames are correct
+    let mut headermap = HeaderMap::with_capacity(1);
+    headermap.append(
+        header::CONTENT_DISPOSITION,
+        HeaderValue::from_str(&format!("attachment; filename=\"{}\"", upload.filename)).unwrap(),
+    );
+
     let buf = fs::read(path).await.expect("could not read uploaded file");
-    Ok(buf)
+    Ok((headermap, buf))
 }
